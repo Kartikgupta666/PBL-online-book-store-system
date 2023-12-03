6f120fdedfaa9c3f9888d155c5f26fa262475e80
kartik gupta <115064547+Kartikgupta666@users.noreply.github.com>
2023-12-03 20:15:33 +0530

    firebase updated successfully

--- online book store/script pages/script.js
+++ online book store/script pages/script.js
@@ -7,14 +7,19 @@ document.querySelector('#close-login-btn').onclick=()=>{
     loginForm.classList.remove('active');
 }
 //registration form
-let registrationForm = document.querySelector('.registration-form-container');
-document.querySelector('#registration-btn').onclick=()=>{
-    registrationForm.classList.toggle('active');
-}
-document.querySelector('#close-registration-btn').onclick=()=>{
-    registrationForm.classList.remove('active');
-}
-// firebase database 
+// not use any more registration form
+// let registrationForm = document.querySelector('.registration-form-container');
+// document.querySelector('#registration-btn').onclick=()=>{
+//     registrationForm.classList.toggle('active');
+// }
+// document.querySelector('#close-registration-btn').onclick=()=>{
+//     registrationForm.classList.remove('active');
+// }
+
+document.getElementById("loginForm").addEventListener("submit",(event)=>{
+    event.preventDefault()
+})
+// fire base code start 
 const firebaseConfig = {
     apiKey: "AIzaSyCngmlr_8LOg5uSsFQ5JjyqIPLB49eQZHs",
     authDomain: "database-f8238.firebaseapp.com",
@@ -25,44 +30,57 @@ const firebaseConfig = {
     appId: "1:864795363220:web:c415e5825789e2e16bb181",
     
   };
-  
-  
-  firebase.initializeApp(firebaseConfig);
-  
-  // Reference messages collection
-  var messagesRef = firebase.database().ref('messages');
-  
-  // Listen for form submit
-  document.getElementById('contactForm').addEventListener('submit', submitForm);
-  
-  // Submit form
-  function submitForm(e){
-    e.preventDefault();
-  
-    // Get values
-     var email = getInputVal('email');
-     var pwd = getInputVal('pwd');
-  
-    // Save message
-    saveMessage( email,pwd);
-  
-    // Hide alert after 3 seconds
-    
-  
-    // Clear form
-    document.getElementById('contactForm').reset();
-  }
-  
-  // Function to get get form values
-  function getInputVal(id){
-    return document.getElementById(id).value;
-  }
-  
-  // Save message to firebase
-  function saveMessage( email, pwd){
-    var newMessageRef = messagesRef.push();
-    newMessageRef.set({
-      email:email,
-      pwd:pwd
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+function login(){
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .catch((error)=>{
+        const errorMessage = error.message;
+        
+        
+    })
+}
+
+function signUp(){
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .catch((error) => {
+        
+        const errorMessage = error.message;
+      alert("Error : " , errorMessage);
+           
     });
-  }
+    var messagesRef = firebase.database().ref('messages');
+    saveMessage( email,password);
+    function getInputVal(id){
+      return document.getElementById(id).value;
+      
+}
+function saveMessage( email, pwd){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    email:email,
+    password:password
+  });
+}
+    
+}
+
+function forgotPass(){
+    const email = document.getElementById("email").value
+    firebase.auth().sendPasswordResetEmail(email)
+    .then(() => {
+        alert("Reset link sent to your email id")
+    })
+    .catch((error) => {
+        document.getElementById("error").innerHTML = error.message
+    });
+}
+
+// ends here firebase...
